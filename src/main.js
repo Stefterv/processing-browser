5dc558d1baec88a20008fcdbb14f2ebb8f17e0e7
Stef Tervelde <stef@steftervelde.nl>
2026-02-02 20:13:12 +0100

    loading OP sketch values

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ if (started) {
   app.quit();
 }
 
-const createWindow = () => {
+const createWindow = async () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -17,12 +17,31 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  // console.log('MAIN_WINDOW_VITE_DEV_SERVER_URL:', MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  // // and load the index.html of the app.
+  // if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+  //   mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  // } else {
+  //   mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  // }
+  mainWindow.loadURL("https://openprocessing.org");
+
+  const res = await fetch(`${MAIN_WINDOW_VITE_DEV_SERVER_URL}/src/plugin.js`, {
+    cache: 'no-store',
+  });
+  const text = await res.text();
+  const base64Text = Buffer.from(text).toString('base64');
+
+  function attachPlugin() {
+    console.log('Attaching plugin.js to mainWindow');
+    mainWindow.webContents.executeJavaScript(`
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = 'data:text/javascript;base64,${base64Text}';
+    document.head.appendChild(script);
+  `);
   }
+  mainWindow.addListener('did-finish-load', attachPlugin);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,30 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import "./index.css"
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // // list
+    // // Preload logic can be added here.
+    // document.body.style.backgroundColor = '#f0f000';
+    // debugger
+    // listen for element with id 'sketchConfig' to be added
+    const observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            const sketchConfig = document.querySelector('#sketchConfig');
+            // console.log('👋 This message is being logged by "preload.js", included via Vite', sketchConfig);
+            var value = sketchConfig?.getAttribute('value');
+            if (value) {
+                try {
+                    const config = JSON.parse(value);
+                    sketchConfig.removeAttribute('value');
+                    console.log('Parsed sketchConfig:', config);
+                } catch (e) {
+                    console.error('Error parsing sketchConfig:', e);
+                }
+            }
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+});
